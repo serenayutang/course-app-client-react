62431ecd70d5c3ff6ecdc03366f971cfe56c5dbb
Serena Tang <serena@Serenas-MacBook-Pro.local>
2021-03-03 23:14:24 -0800

    update index

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import '@fortawesome/fontawesome-free/css/all.min.css'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CounterReact from "./components/counter/react-state/counter-react";
+import CounterRedux from "./components/counter/redux-state/counter-redux";
 
 ReactDOM.render(
   <React.StrictMode>
     <App/>
+    {/*<CounterRedux/>*/}
   </React.StrictMode>,
   document.getElementById('root')
 );
