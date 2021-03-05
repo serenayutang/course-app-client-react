44eeb0530173130d44188e0b8b547a4bf08b4874
Serena Tang <serena@Serenas-MacBook-Pro.local>
2021-03-05 00:22:28 -0800

    add combineReducers

--- src/components/course-editor/course-editor.js
+++ src/components/course-editor/course-editor.js
@@ -3,12 +3,21 @@ import "./course-editor.css";
 import {useHistory} from "react-router-dom";
 import {Link} from "react-router-dom";
 import moduleReducer from "../../reducers/modules-reducer";
-import {createStore} from "redux";
+import lessonReducer from "../../reducers/lesson-reducer";
+import {combineReducers, createStore} from "redux";
 import {Provider} from "react-redux";
 import ModuleList from "./module-list";
+import LessonTabs from "./lesson-tabs";
+
+const reducer = combineReducers({
+    moduleReducer: moduleReducer,
+    lessonReducer: lessonReducer
+})
 
 
-const store = createStore(moduleReducer)
+// const store = createStore(moduleReducer)
+// const store = createStore(lessonReducer)
+const store = createStore(reducer)
 
 const CourseEditor = ({history}) =>
     <Provider store={store}>
@@ -22,7 +31,12 @@ const CourseEditor = ({history}) =>
                 <i onClick={() => history.goBack()}
                    className="fas fa-times float-right"></i>
             </h2>
-            <ModuleList/>
+            <div className="row">
+                <div className="col-4"><ModuleList/></div>
+                <div className="col-8"><LessonTabs/></div>
+            </div>
+
+
 
         </div>
     </Provider>
