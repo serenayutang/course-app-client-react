84c125b9732f965808fbed683249c50e004717c0
Serena Tang <serena@Serenas-MacBook-Pro.local>
2021-03-05 00:19:34 -0800

    add connection to update and delete module

--- src/components/course-editor/module-list.js
+++ src/components/course-editor/module-list.js
@@ -1,28 +1,51 @@
 import React from "react";
 import {connect} from "react-redux";
+import EditableItem from "./editable-item";
 
-const ModuleList = ({myModules=[]}) =>
+const ModuleList = (
+    {
+        myModules=[],
+        createModule,
+        deleteModule = (item) => alert("delete" + item.id),
+        updateModule
+    }) =>
     <div>
         <h2>Modules {myModules.length}</h2>
         <ul className="list-group">
             {
                 myModules.map(module =>
                 <li className="list-group-item">
-                    {module.title}
+                    <EditableItem
+                        updateItem={updateModule}
+                        deleteItem={deleteModule}
+                        item={module}/>
                 </li>
                 )
             }
+            <li className="list-group-item">
+                <i onClick={createModule} className="fas fa-plus fa-2x"/>
+            </li>
         </ul>
     </div>
 
 const stateToProperyMapper = (state) => {
     return {
-        myModules: state.modules
+        myModules: state.moduleReducer.modules
     }
 }
 
 const dispatchToPropertyMapper = (dispatch) => {
-
+    return {
+        createModule: () => dispatch({type: "CREATE_MODULE"}),
+        deleteModule: (item) => dispatch({
+            type: "DELETE_MODULE",
+            moduleToDelete: item
+        }),
+        updateModule: (module) => dispatch ({
+            type: "UPDATE_MODULE",
+            module: module
+        })
+    }
 }
 
 export default connect(stateToProperyMapper, dispatchToPropertyMapper) (ModuleList)
