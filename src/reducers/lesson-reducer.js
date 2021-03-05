c2d2ca04abd244a815bd2d7ec371e6e7af49d1fb
Serena Tang <serena@Serenas-MacBook-Pro.local>
2021-03-05 00:16:33 -0800

    add lesson reducer

---
+++ src/reducers/lesson-reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    lessons: [
+        {_id: "444", title: "Lesson AA"},
+        {_id: "555", title: "Lesson BB"},
+        {_id: "666", title: "Lesson CC"}
+    ]
+}
+
+const lessonReducer = (state = initialState, action) => {
+    return state
+}
+export default lessonReducer
+
