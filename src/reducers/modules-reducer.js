7ba15e4dad606d48ae38a3c2e876f0dc73b4f533
Serena Tang <serena@Serenas-MacBook-Pro.local>
2021-03-05 00:21:09 -0800

    add create/delete/update module type

--- src/reducers/modules-reducer.js
+++ src/reducers/modules-reducer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {act} from "@testing-library/react";
 
 
 const initialState = {
@@ -12,8 +13,40 @@ const initialState = {
 const moduleReducer = (state=initialState, action) => {
     switch (action.type) {
         case "CREATE_MODULE":
+            const newState1 = {
+                modules: [
+                    ...state.modules,
+                    {
+                        title: "NEW MODULE",
+                        _id: (new Date()).getTime()
+                    }
+                ]
+            }
+            return newState1
+
         case "DELETE_MODULE":
+            alert("delete" + action.moduleToDelete.title)
+            const newState2 = {
+                modules: state.modules.filter(module => {
+                    if(module._id === action.moduleToDelete._id) {
+                        return false
+                    } else {
+                        return true
+                    }
+                })
+            }
+            return newState2
+
         case "UPDATE_MODULE":
+            return {
+                modules: state.modules.map(m => {
+                    if (m._id === action.module._id) {
+                        return action.module
+                    } else {
+                        return m
+                    }
+                })
+            }
         default:
             return state
     }
