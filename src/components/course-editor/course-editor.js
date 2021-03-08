d62547a251b6887c68a94d0d305b81e99cf12cd1
Serena Tang <serena@Serenas-MacBook-Pro.local>
2021-03-07 20:53:07 -0800

    add courseId/moduleId useParams

--- src/components/course-editor/course-editor.js
+++ src/components/course-editor/course-editor.js
@@ -1,45 +1,44 @@
 import React from 'react'
 import "./course-editor.css";
-import {useHistory} from "react-router-dom";
-import {Link} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import moduleReducer from "../../reducers/modules-reducer";
 import lessonReducer from "../../reducers/lesson-reducer";
 import {combineReducers, createStore} from "redux";
 import {Provider} from "react-redux";
 import ModuleList from "./module-list";
 import LessonTabs from "./lesson-tabs";
+import TopicPills from "./topic-pills";
 
 const reducer = combineReducers({
     moduleReducer: moduleReducer,
     lessonReducer: lessonReducer
 })
 
-
-// const store = createStore(moduleReducer)
-// const store = createStore(lessonReducer)
 const store = createStore(reducer)
 
-const CourseEditor = ({history}) =>
-    <Provider store={store}>
-        <div>
-            <h2>
-                <Link to = "/courses/table">
-                    <i className="fas fa-arrow-left"></i>
-                </Link>
-
-                Course Editor
-                <i onClick={() => history.goBack()}
-                   className="fas fa-times float-right"></i>
-            </h2>
-            <div className="row">
-                <div className="col-4"><ModuleList/></div>
-                <div className="col-8"><LessonTabs/></div>
+const CourseEditor = ({history}) => {
+    const {courseId, moduleId} = useParams();
+    return (
+        <Provider store={store}>
+            <div>
+                <h2>
+                    {/*<Link to = "/courses/table">*/}
+                    {/*    <i className="fas fa-arrow-left"></i>*/}
+                    {/*</Link>*/}
+                    <Link>
+                        <i onClick={() => history.goBack()} className="fas fa-arrow-left"/>
+                    </Link>
+                    Course Editor
+                    <i onClick={() => history.goBack()} className="fas fa-times float-right"/>
+                </h2>
+                <div className="row">
+                    <div className="col-4"><ModuleList/></div>
+                    <div className="col-8"><LessonTabs/></div>
+                </div>
             </div>
-
-
-
-        </div>
-    </Provider>
+        </Provider>
+    )
+}
 export default CourseEditor
 
 // const CourseEditor = () => {
