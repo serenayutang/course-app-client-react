d0f04269d999d805c56adade89408adb913a5a12
Serena Tang <serena@Serenas-MacBook-Pro.local>
2021-03-21 19:49:39 -0700

    add Widget List

--- src/components/course-editor/course-editor.js
+++ src/components/course-editor/course-editor.js
@@ -4,17 +4,20 @@ import {Link, useParams} from "react-router-dom";
 import moduleReducer from "../../reducers/modules-reducer";
 import lessonReducer from "../../reducers/lesson-reducer";
 import topicReducer from "../../reducers/topic-reducer";
+import widgetReducer from "../../reducers/widget-reducer";
 import {combineReducers, createStore} from "redux";
 import {Provider} from "react-redux";
 import ModuleList from "./module-list";
 import LessonTabs from "./lesson-tabs";
 import TopicPills from "./topic-pills";
+import WidgetList from "../widgets/widget-list";
 
 
 const reducer = combineReducers({
     moduleReducer: moduleReducer,
     lessonReducer: lessonReducer,
-    topicReducer: topicReducer
+    topicReducer: topicReducer,
+    widgetReducer: widgetReducer
 })
 
 const store = createStore(reducer)
@@ -37,6 +40,8 @@ const CourseEditor = () => {
                         <LessonTabs/>
                         <br/>
                         <TopicPills/>
+                        <br/>
+                        <WidgetList/>
                     </div>
                 </div>
             </div>
